20521b27bd62de89b09a2b9432124f0dd5fa4ceb
Jaenyeong-kim <jaenyeong.dev@gmail.com>
2020-07-26 23:48:08 +0900

    remove unused import

--- src/test/java/com/jaenyeong/springboot_started/test/SampleControllerTest.java
+++ src/test/java/com/jaenyeong/springboot_started/test/SampleControllerTest.java
@@ -8,15 +8,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.system.OutputCaptureRule;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
 // 서블릿 컨테이너를 mock up 해서 mocking된 디스패처 서블릿이 로딩
